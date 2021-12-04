5d4b9d7a9d188b22bedabd853636bc237bac6e0f
Michael Belousov <mike.belousov@bentley.com>
2021-12-03 23:28:58 -0500

    realized window should be used instead of app for a cloneable event-emitting api

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -15,7 +15,7 @@ use std::{
   cell::RefCell,
 };
 
-#[derive(serde::Serialize)]
+#[derive(serde::Serialize, Clone)]
 struct Message {
   channel: String,
   json: String,
@@ -40,48 +40,52 @@ fn main() {
       ipc: Arc::new(tokio::sync::Mutex::new(None)),
       child: Arc::new(Mutex::new(None)),
     })
-    .invoke_handler(tauri::generate_handler![ipcRenderer_invoke, ipcRenderer_send])
-    .setup(move |app| {
+    //.invoke_handler(tauri::generate_handler![ipcRenderer_invoke, ipcRenderer_send])
+    .setup(|app| {
+
+      let (mut rx, mut child) = Command::new_sidecar("node")
+        // TODO: go back to using `pkg` to package the node.js code as v8 bytecode for startup performance and hiding source
+        .expect("failed to setup `node` sidecar")
+        .args(&["binaries/dist/main.js"])
+        .spawn()
+        .expect("Failed to spawn packaged node");
+
       let window = app.get_window("main").unwrap();
 
-      tauri::async_runtime::spawn(async move {
-        let (mut rx, mut child) = Command::new_sidecar("node")
-          // TODO: go back to using `pkg` to package the node.js code as v8 bytecode for startup performance and hiding source
-          .expect("failed to setup `node` sidecar")
-          .args(&["binaries/dist/main.js"])
-          .spawn()
-          .expect("Failed to spawn packaged node");
+      let child_cell = RefCell::new(child);
 
-        app.listen_global("ipcRenderer_invoke", |event| {
-          tauri::async_runtime::spawn(async move {
-            child.write(event.payload().as_bytes()).unwrap();
-            child.write("\n".as_bytes()).unwrap();
-            if let Some(cmd) = rx.recv().await {
-              app.emit_all("ipcRenderer_invoke",
-                match event {
-                  CommandEvent::Stdout(json) => {
-                    println!("stdout! '{}'", line);
-                    Message { json, channel: "ipcRenderer_invoke" }
-                  }
-                  CommandEvent::Stderr(json) => {
-                    println!("stderr! '{}'", line);
-                    Message { json, channel: "ipcRenderer_invoke_err" }
-                  }
-                  CommandEvent::Terminated(payload) => {
-                    println!("terminated! '{:?}'", payload);
-                    Err("sidecar terminated".into())
-                  }
-                  _ => {
-                    println!("unknown!");
-                    Err("unknown command event".into())
-                  }
-                }
-              );
-            } else {
-              println!("error child had None response")
+      window.listen("ipcRenderer_event", move |event| {
+        child_cell.borrow_mut().write(event.payload().unwrap_or("<none>").as_bytes()).unwrap();
+        child_cell.borrow_mut().write("\n".as_bytes()).unwrap();
+      });
+
+      tauri::async_runtime::spawn(async move {
+        if let Some(cmd) = rx.recv().await {
+          window.emit("ipcRenderer_invoke",
+            match cmd {
+              CommandEvent::Stdout(json) => {
+                println!("stdout! '{}'", json);
+                Message { json, channel: "ipcRenderer_invoke".into() }
+              }
+              CommandEvent::Stderr(json) => {
+                println!("stderr! '{}'", json);
+                Message { json, channel: "ipcRenderer_invoke_err".into() }
+              }
+              CommandEvent::Terminated(payload) => {
+                println!("terminated! '{:?}'", payload);
+                //Err("sidecar terminated".into())
+                Message { json: "terminated!".into(), channel: "ipcRenderer_invoke_term".into() }
+              }
+              _ => {
+                println!("unknown!");
+                //Err("unknown command event".into())
+                Message { json: "unknown!".into(), channel: "ipcRenderer_invoke_unknown".into() }
+              }
             }
-          });
-        });
+          );
+        } else {
+          println!("error child had None response")
+        }
       });
 
       Ok(())
