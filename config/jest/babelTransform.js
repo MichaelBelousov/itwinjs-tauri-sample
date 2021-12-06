f5b4b53746f44577d4d7f8eba6ea029c2a1982e1
Michael Belousov <mike.belousov@bentley.com>
2021-12-06 18:49:38 -0500

    ejected

---
+++ config/jest/babelTransform.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const babelJest = require('babel-jest');
+
+const hasJsxRuntime = (() => {
+  if (process.env.DISABLE_NEW_JSX_TRANSFORM === 'true') {
+    return false;
+  }
+
+  try {
+    require.resolve('react/jsx-runtime');
+    return true;
+  } catch (e) {
+    return false;
+  }
+})();
+
+module.exports = babelJest.createTransformer({
+  presets: [
+    [
+      require.resolve('babel-preset-react-app'),
+      {
+        runtime: hasJsxRuntime ? 'automatic' : 'classic',
+      },
+    ],
+  ],
+  babelrc: false,
+  configFile: false,
+  // With the added support for svg-sprites using the resource query '?sprite', this
+  // plugin is needed to remove the query for imports during tests.
+  // Adding the second plugin so tests don't break if some package decides to use raw-loader
+  plugins: [
+    'babel-plugin-import-remove-resource-query',
+    ['strip-requirejs-plugin-prefix', { plugin: 'raw-loader' }],
+  ],
+});
