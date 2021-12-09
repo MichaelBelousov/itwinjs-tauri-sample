834f722953353d67b1691e94caf71675704139fa
Michael Belousov <mike.belousov@bentley.com>
2021-12-08 20:15:45 -0500

    fix tauri not understanding itwin webpack content isn't esm

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -397,15 +397,16 @@ module.exports = function (webpackEnv) {
       // https://twitter.com/wSokra/status/969633336732905474
       // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
       splitChunks: {
-        chunks: 'all',
-        name: isEnvDevelopment,
+        //chunks: 'all',
+        //name: isEnvDevelopment,
+        cacheGroups: {
+          default: false
+        }
       },
       // Keep the runtime chunk separated to enable long term caching
       // https://twitter.com/wSokra/status/969679223278505985
       // https://github.com/facebook/create-react-app/issues/5358
-      runtimeChunk: {
-        name: entrypoint => `runtime-${entrypoint.name}`,
-      },
+      runtimeChunk: false,
     },
     resolve: {
       symlinks: process.env.SYMLINKS !== 'false',
